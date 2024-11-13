4a613f402ea8042f6d9730cec3687d6ceaccfb76
Bisma <bob@Bismas-MacBook-Air.local>
2024-11-13 15:38:15 -0800

    Vercel Production error resolved

--- config/db.js
+++ config/db.js
@@ -1,17 +1,25 @@
-
 import mongoose from 'mongoose';
 
+let isConnected = false;
 
 const connectDB = async () => {
+  if (isConnected) {
+    console.log("Using existing MongoDB connection.");
+    return;
+  }
+
   try {
+    // Establish a new connection if not connected yet
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+
+    isConnected = true;
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
-    process.exit(1);
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);  // Exit the process if connection fails
   }
 };
 
