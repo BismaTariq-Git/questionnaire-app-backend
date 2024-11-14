6e39ded353ec4d0f43d48c67afc42d56d0591809
Bisma <bob@Bismas-MacBook-Air.local>
2024-11-13 17:27:52 -0800

    vercel builds

--- server.js
+++ server.js
@@ -27,13 +27,15 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');  // Allow all origins
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');  // Allow content-type and other headers
+    
     // Handle preflight requests
     if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);  // Send OK for OPTIONS request
+        return res.sendStatus(200);  // Send OK for OPTIONS request
     }
-  
+    
     next();
-  });
+});
 
 
 app.use((req, res, next) => {
