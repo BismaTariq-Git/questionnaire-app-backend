38978d4bebb0a8128f7420b913babcd19bcab212
Bisma <bob@Bismas-MacBook-Air.local>
2024-11-13 17:44:38 -0800

    vercel builds

--- routes/surveyroutes.js
+++ routes/surveyroutes.js
@@ -5,18 +5,6 @@ import { createOrUpdateSurvey, getSurvey, finalizeSurvey } from '../controllers/
 
 const router = express.Router();
 
-// Allow CORS for the /survey and /submit-survey endpoints
-router.options('/survey', cors());
-router.options('/survey/progress', cors());
-
-// Add the explicit OPTIONS handler for /submit-survey endpoint
-router.options('/survey/submit-survey', (req, res) => {
-  res.set('Access-Control-Allow-Origin', req.headers.origin || '*');
-  res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.set('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.sendStatus(200);
-});
-
 // Define the routes
 router.post('/survey', createOrUpdateSurvey);
 router.get('/survey/progress', getSurvey);
